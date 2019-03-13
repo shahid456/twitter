6a98a7fb8cadcf49313c9139d48503ee493dcdd4
Shahid Jamil <shahid.jamil@emumba.com>
2019-03-13 14:43:10 +0500

    new files

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import LandingPage from "./landingPage/HomePage.js";
+
+
+ReactDOM.render(<LandingPage />, document.getElementById("root"));
